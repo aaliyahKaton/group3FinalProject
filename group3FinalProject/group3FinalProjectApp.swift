c9aaccd7abbc5565965abab45edc9a0bd77055e6
Aaliyah <aaliyah.katon@hotmail.co.uk>
2024-08-13 10:04:47 +0100

    Initial Commit

---
+++ group3FinalProject/group3FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  group3FinalProjectApp.swift
+//  group3FinalProject
+//
+//  Created by Scholar on 13/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct group3FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
